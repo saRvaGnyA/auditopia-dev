172195d0c7812db96a0464708f15e48e962c130e
Chaitya <chaityavora02@gmail.com>
2023-10-22 18:57:28 +0530

    JuryAllocation implemented using ChainLink VRF

---
+++ hardhat/contracts/JuryAllocation.sol
@@ -0,0 +1,106 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/shared/access/ConfirmedOwner.sol";
+
+contract JuryAllocation is VRFConsumerBaseV2, ConfirmedOwner {
+    event RequestSent(uint256 requestId, uint32 numWords);
+    event RequestFulfilled(uint256 requestId, uint256[] randomWords);
+
+    struct RequestStatus {
+        bool fulfilled; 
+        bool exists; 
+        uint256[] randomWords;
+    }
+    mapping(uint256 => RequestStatus)
+        public s_requests; 
+    VRFCoordinatorV2Interface COORDINATOR;
+
+    uint64 s_subscriptionId;
+    uint256[] public requestIds;
+    uint256 public lastRequestId;
+
+    bytes32 keyHash =
+        0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c;
+
+    uint32 callbackGasLimit = 100000;
+
+    // The default is 3, but you can set this higher.
+    uint16 requestConfirmations = 3;
+
+    //randomly selecting 5 juries from list of juries
+    uint32 numWords = 5;
+    mapping(uint256=>uint256[]) public requestIdToRandomJury;
+
+    constructor(
+        uint64 subscriptionId
+    )
+        VRFConsumerBaseV2(0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625)
+        ConfirmedOwner(msg.sender)
+    {
+        COORDINATOR = VRFCoordinatorV2Interface(
+            0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625
+        );
+        s_subscriptionId = subscriptionId;
+    }
+
+    function requestRandomWords()
+        external
+        onlyOwner
+        returns (uint256 requestId)
+    {
+        requestId = COORDINATOR.requestRandomWords(
+            keyHash,
+            s_subscriptionId,
+            requestConfirmations,
+            callbackGasLimit,
+            numWords
+        );
+        s_requests[requestId] = RequestStatus({
+            randomWords: new uint256[](0),
+            exists: true,
+            fulfilled: false
+        });
+        requestIds.push(requestId);
+        lastRequestId = requestId;
+        emit RequestSent(requestId, numWords);
+        return requestId;
+    }
+
+    function fulfillRandomWords(
+        uint256 _requestId,
+        uint256[] memory _randomWords
+    ) internal override {
+        require(s_requests[_requestId].exists, "request not found");
+        s_requests[_requestId].fulfilled = true;
+        s_requests[_requestId].randomWords = _randomWords;
+        emit RequestFulfilled(_requestId, _randomWords);
+    }
+
+    function getRequestStatus(
+        uint256 _requestId
+    ) external returns (bool fulfilled, uint256[] memory randomWords,uint256[] memory randomJury) {
+        require(s_requests[_requestId].exists, "request not found");
+        RequestStatus memory request = s_requests[_requestId];
+        uint256[] memory randomJury=new uint256[](5);
+        uint256[] memory temp=new uint256[](25);
+        for(uint256 i=0;i<25;i++){
+            temp[i]=i+1;
+        }
+        // converting random uint256 generated number to number 
+        // between 0-maxNumberOfAvailableJury
+        for(uint256 i=0;i<5;i++){
+            uint256 minValue = 0;
+            uint256 maxValue = 25;
+            uint256 randomNumber=((request.randomWords[i] % (maxValue - minValue + 1)) + minValue);
+            randomJury[i]=randomNumber;
+            temp[i]=temp[maxValue-1];
+            temp[maxValue-1]=0;
+            maxValue=maxValue-1;
+        }
+        requestIdToRandomJury[_requestId]=randomJury;
+        return (request.fulfilled, request.randomWords,randomJury);
+    }
+}
